70096799ae0a1b4e588756f280fac88e83b7d6ca
jcqb09 <jcqb0909@gmail.com>
2022-05-23 15:27:07 -0400

    react router -> made weather and news app render separately

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import NewsApp from "./NewsApp";
 import WeatherApp from "./WeatherApp";
 import Header from "./Header";
+import Error from "./Error";
 
 function App() {
   return (
-    <div style={{ backgroundColor: "#FFE6D8" }}>
-      <>
-        <Header />
-        <WeatherApp />
-        <NewsApp />
-      </>
-    </div>
+    <main style={{ backgroundColor: "#FFE6D8" }}>
+      <Header />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/weather">Weather</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" />
+        <Route path="/weather" element={<WeatherApp />} />
+        <Route path="/news" element={<NewsApp />} />
+        <Route element={Error} />
+      </Routes>
+    </main>
   );
 }
 
